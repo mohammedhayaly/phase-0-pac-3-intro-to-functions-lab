854d7132f1f6e79e94eeb0a2c18086db1534dd44
mohammedhayaly <mohammed.alhayali88@yahoo.com>
2023-04-10 04:25:53 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,75 @@
+
+
+
+
+'hello!'.toUpperCase()
+
+function shout(string) {
+  return string.toUpperCase()
+  
+  
+}
+
+
+
+
+'HELLO!'.toLowerCase()
+
+function whisper(string) {
+  return string.toLowerCase()
+  
+}
+
+
+
+
+function logShout(string){
+  return "hello".toUpperCase;
+ 
+}
+console.log(string.toUpperCase); 
+
+
+
+
+
+function logWhisper(string){
+  return "HELLO".toLowerCase;
+ 
+}
+console.log(string.toLowerCase(hello)); 
+
+
+
+
+
+"I can't hear you!".sayHiToHeadphonedRoommate()
+
+function sayHiToHeadphonedRoommate(string) {
+  return string.sayHiToHeadphonedRoommate()
+}
+
+
+
+
+
+
+
+
+let string = "hello";
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+
+logShout();
+
+
+
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+
+logWhisper();
